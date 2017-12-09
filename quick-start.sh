a8d029ffdfffcac1d576d320110c66bc1e4aee47
Ryan Noelk <ryannoelk@gmail.com>
2017-12-09 12:13:56 -0500

    Update quick-start.sh

--- quick-start.sh
+++ quick-start.sh
@@ -2,11 +2,11 @@
 
 echo "OpenEats quick setup script"
 
+echo "Take some backups"
+docker exec openeats_db_1 sh -c 'exec mysqldump openeats -uroot -p"$MYSQL_ROOT_PASSWORD"' > openeats.sql
+
 echo "Downloading docker-compose.yml"
 wget https://github.com/RyanNoelk/OpenEats/blob/master/docker-prod.yml
-
-echo "Take some backups"
-https://github.com/RyanNoelk/OpenEats/blob/master/docker-prod.yml
 
 echo "Downloading Images"
 docker pull openeats/api
@@ -22,7 +22,7 @@ sleep 15
 if [[ $? -eq "1" ]]; then
    curl -vvvv http://127.0.0.1/api/v1
    echo ""
-   echo "If the above curl command was a success then you're all set!"
+   echo "If the above curl command was successful then you're all set!"
 else
    echo "Something failed, please check and see what it was."
    exit 1
